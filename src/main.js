4745da4c3205e556ece566226759877c5718eaf1
void-echo <beihai0616@gmail.com>
2022-08-05 14:52:42 +0800

    Initial Commit

--- src/main.js
+++ src/main.js
@@ -1,8 +1,16 @@
 import Vue from 'vue'
+import './plugins/axios'
 import App from './App.vue'
+import BaiduMap from 'vue-baidu-map'
 
 Vue.config.productionTip = false
 
+Vue.use(BaiduMap, {
+  ak: '2k5OisLCPPfHTLjCuAv6SjaR6rbOs9Yv'
+})
+
+
 new Vue({
   render: h => h(App),
-}).$mount('#app')
+}).$mount('#app');
+
